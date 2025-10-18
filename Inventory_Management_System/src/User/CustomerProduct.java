efe712abc3e2baf777d2ccb8dee872c859f49298
Abdalla Elmougi <abdullah.elmogy@gmail.com>
2025-10-19 01:36:18 +0300

    Refactor  for the new validation

--- Inventory_Management_System/src/User/CustomerProduct.java
+++ Inventory_Management_System/src/User/CustomerProduct.java
@@ -3,80 +3,67 @@ package User;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import Database.ObjectInfo;
+import MyUtilities.Validation;
 
-public class CustomerProduct implements ObjectInfo
-{
+public class CustomerProduct implements ObjectInfo {
     private String customerSSN;
     private String productID;
     private LocalDate purchaseDate;
     private boolean paid;
 
-    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate)
-    {
-        if (customerSSN == null || customerSSN.isEmpty())
-        {
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        if (!Validation.isValidString(customerSSN)) {
             throw new IllegalArgumentException("Invalid customer SSN.");
         }
-        if (productID == null || productID.isEmpty())
-        {
+        if (!Validation.isValidString(productID)) {
             throw new IllegalArgumentException("Invalid product ID.");
         }
-        if (purchaseDate == null)
-        {
+        if (purchaseDate == null) {
             throw new IllegalArgumentException("Invalid purchase date.");
         } 
 
-        this.customerSSN=customerSSN;
-        this.productID=productID;
-        this.purchaseDate=purchaseDate;
-        paid=false;
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
     } 
     
-    public String getCustomerSSN()
-    {
+    public String getCustomerSSN() {
         return customerSSN;
     } 
     
-    public String getProductID()
-    {
+    public String getProductID() {
         return productID;
     } 
     
-    public LocalDate getPurchaseDate()
-    {
+    public LocalDate getPurchaseDate() {
         return purchaseDate;
     }
     
-    public boolean isPaid()
-    {
+    public boolean isPaid() {
         return paid;
     }
     
-    public void setPaid(boolean paid)
-    {
-        this.paid=paid;
+    public void setPaid(boolean paid) {
+        this.paid = paid;
     }
     
     @Override
-    public String lineRepresentation()
-    {
-        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public String lineRepresentation() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return (customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid);
     }
     
     @Override
-    public String getSearchKey()
-    {
-        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    public String getSearchKey() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return (customerSSN + "," + productID + "," + purchaseDate.format(formatter));
     }
 
-    public static void main(String[]args)
-    {
+    public static void main(String[] args) {
         CustomerProduct c = new CustomerProduct("null", "P2568", LocalDate.of(2022, 2, 12));
         c.setPaid(true);
         System.out.println(c.lineRepresentation());
         System.out.println(c.getSearchKey());
-
     }
 }
