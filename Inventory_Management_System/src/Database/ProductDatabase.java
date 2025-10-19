35145d7cf4b263076266bba1c86196805d887d42
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-19 07:28:25 +0300

    started the employee role

--- Inventory_Management_System/src/Database/ProductDatabase.java
+++ Inventory_Management_System/src/Database/ProductDatabase.java
@@ -1,26 +1,20 @@
 package Database;
 
-
 import User.Product;
 
-public class ProductDatabase extends DatabaseTemplate <Product> {
+public class ProductDatabase extends DatabaseTemplate<Product> {
 
-    public ProductDatabase (String filename){
+    public ProductDatabase(String filename) {
         super(filename);
     }
 
-    
     @Override
-    public Product createRecordFrom(String line){
-     
+    public Product createRecordFrom(String line) {
+
         String[] fields = line.split(",");
 
-return new Product(fields[0], fields[1] , fields[2], fields[3], Integer.parseInt(fields[4]), Float.parseFloat(fields[5]));
+        return new Product(fields[0], fields[1], fields[2], fields[3], Integer.parseInt(fields[4]),
+                Float.parseFloat(fields[5]));
     }
 
-
-
-
 }
-    
-
--- Inventory_Management_System/src/User/EmployeeRole.java
+++ Inventory_Management_System/src/User/EmployeeRole.java
@@ -1,5 +1,38 @@
 package User;
 
-public class EmployeeRole {
+import Database.*;
 
+public class EmployeeRole {
+    private ProductDatabase productsDatabase;
+    private CustomerProductDatabase customerProductDatabase;
+
+    public EmployeeRole() {
+        productsDatabase = new ProductDatabase("Products.txt");
+        try {
+            productsDatabase.readFromFile();
+        } catch (Exception e) {
+            System.err.println("Error reading Products.txt: " + e.getMessage());
+        }
+
+        customerProductDatabase = new CustomerProductDatabase("CustomerProducts.txt");
+        try {
+            customerProductDatabase.readFromFile();
+        } catch (Exception e) {
+            System.err.println("Error reading CustomerProducts.txt: " + e.getMessage());
+        }
+    }
+
+    public void addProduct(String productID, String productName, String manufacturerName, String supplierName, int quantity) {
+        Product product = new Product(productID, productName, manufacturerName, supplierName, quantity, 0.0f);
+        productsDatabase.insertRecord(product);
+
+        try {
+            productsDatabase.saveToFile();
+        } catch (Exception e) {
+            System.err.println("Error saving product to file: " + e.getMessage());
+        }
+    }
+
+    public Product[] getListOfProducts(){
+    }
 }
